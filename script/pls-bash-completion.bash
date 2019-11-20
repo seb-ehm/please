17a398f611ef34d286957e084d8cf083e5581c44
Sebastian Ehmann <sebastian.ehmann1@volkswagen.de>
2019-11-20 20:10:50 +0100

    Remove leading tab caused by fc command

--- script/pls-bash-completion.bash
+++ script/pls-bash-completion.bash
@@ -5,9 +5,11 @@ _pls_complete()
     current_word="${COMP_WORDS[COMP_CWORD]}"
 
     last_command=`fc -ln -0`
+    last_command="${last_command#"${last_command%%[![:space:]]*}"}"
+    echo "Last Command: $last_command" >> "/mnt/c/src/please/bashruns.txt"
     local IFS=$'\n'
-    suggestion=`please "${last_command}"`
-
+    suggestion=`please "$last_command"`
+    echo "$suggestion" >> "/mnt/c/src/please/bashruns.txt"
     COMPREPLY=( $(compgen -W "${suggestion}" -- $current_word ) );
     return 0
 }
