fcbaae649403c56100af33903492ab0b87b5457a
Sebastian Ehmann <sebastian@dipolmoment.de>
2019-11-21 20:54:39 +0100

    Change location of debug output file

--- script/pls-bash-completion.bash
+++ script/pls-bash-completion.bash
@@ -6,10 +6,10 @@ _pls_complete()
 
     last_command=`fc -ln -0`
     last_command="${last_command#"${last_command%%[![:space:]]*}"}"
-    echo "Last Command: $last_command" >> "/mnt/c/src/please/bashruns.txt"
+    echo "Last Command: $last_command" >> "bashruns.txt"
     local IFS=$'\n'
     suggestion=`please "$last_command"`
-    echo "$suggestion" >> "/mnt/c/src/please/bashruns.txt"
+    echo "$suggestion" >> "bashruns.txt"
     COMPREPLY=( $(compgen -W "${suggestion}" -- $current_word ) );
     return 0
 }
